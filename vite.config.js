18bfca0efc88cd2821bfafdd519f980e13816e6b
gusgodho <gusgodho@gmail.com>
2025-10-24 19:17:35 -0600

    Add base path for deployment in vite config
    Set base path for deployment to match repository name.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/traductor-tiempo-real/', // ⚠️ IMPORTANTE: debe coincidir con el nombre de tu repositorio
   server: {
     port: 5173,
     open: true
